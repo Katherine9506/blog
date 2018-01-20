e94b0aa3d06ecaa8823b300240607163ac96210b
kath <1134052988@qq.com>
2018-01-21 02:13:17 +0800

    文章模块

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,23 +39,41 @@ class PostController extends Controller
     }
 
     //编辑页面
-    public function edit()
+    public function edit(Post $post)
     {
-        return view("post/edit");
+        return view("post/edit", compact('post'));
 
     }
 
     //编辑逻辑
-    public function update()
+    public function update(Post $post, Request $request)
     {
-        return;
+        $this->validate($request, [
+            'title' => 'required|string|max:100|min:5',
+            'content' => 'required|string|min:10',
+        ]);
+        $post->title = $request->get('title');
+        $post->content = $request->get('content');
+        $post->save();
 
+        return redirect("/posts/{$post->id}");
     }
 
     //删除逻辑
-    public function delete()
+    public function delete(Post $post)
     {
-        return;
+        // TODO:用户权限的验证
+
+        $post->delete();
+
+        return redirect("/posts");
+    }
+
+    //上传图片
+    public function imageUpload(Request $request)
+    {
+        $path = $request->file('wangEditorH5File')->storePublicly(md5(time()));
+        return asset('storage/' . $path);
 
     }
 }
